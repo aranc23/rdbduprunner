4f2275ec72945464b4a4fcb750d9a27159d93fca
Aran Cox <aran-cox@uiowa.edu>
2022-10-13 20:59:32 -0500

    make rpm/debs include another module and /run/rdbduprunner plus tmpfiles.d

--- makefpm.sh
+++ makefpm.sh
@@ -2,16 +2,17 @@
 #
 pkg=rdbduprunner
 rm -rf ./Build/*
-mkdir -p ./Build/{usr/bin,"usr/lib/${pkg}","etc/${pkg}/conf.d","var/run/${pkg}","var/log/${pkg}"} 
+mkdir -p ./Build/{usr/bin,"usr/lib/${pkg}","usr/lib/tmpfiles.d","etc/${pkg}/conf.d","run/${pkg}","var/log/${pkg}"} 
 
-cp rdbduprunner ./Build/usr/bin/
+cp $pkg ./Build/usr/bin/
 rsync -av check_mk "./Build/usr/lib/${pkg}"
+cp "contrib/tmpfiles.d/${pkg}.conf" ./Build/usr/lib/tmpfiles.d/
 
 version=$(git tag -l | tail -1 | sed 's/^v//' )
 summary="script for making backups with rsync, rdiff-backup, and duplicity"
 description="runs backup programs per a configuration file"
-rpm_deps="-d perl-JSON -d perl-Log-Dispatch -d perl-Config-General"
-deb_deps="-d libjson-perl -d liblog-dispatch-perl -d libconfig-general-perl"
+rpm_deps="-d perl-JSON -d perl-Log-Dispatch -d perl-Config-General -d perl-Readonly"
+deb_deps="-d libjson-perl -d liblog-dispatch-perl -d libconfig-general-perl -d libreadonly-perl"
 common_deps="-d perl -d rsync"
 common_opts="-n rdbduprunner --version "${version}" --architecture noarch -C ./Build -s dir"
 
