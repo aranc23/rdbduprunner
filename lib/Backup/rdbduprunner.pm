76c3a6c6571276d61a634f45d5a453570246c8e8
Aran Cox <aran-cox@uiowa.edu>
2022-10-22 13:17:01 -0500

    move the rsync exit codes to the module

--- lib/Backup/rdbduprunner.pm
+++ lib/Backup/rdbduprunner.pm
@@ -13,6 +13,7 @@ use Log::Dispatch::Syslog;
 use Log::Dispatch::Screen;
 use Log::Dispatch::File;
 use POSIX qw( strftime pause );
+use Readonly;
 
 our @ISA = qw(Exporter);
 
@@ -34,6 +35,7 @@ critical
 alert
 emergency
 dlog
+$EXIT_CODE
  ) ] );
 
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
@@ -43,6 +45,32 @@ our @EXPORT = qw(
 );
 
 our $VERSION = '1.4.5';
+
+# from the man page for rsync 3.1.1
+Readonly our $EXIT_CODE => {
+    'rsync' => {
+        '0'  => 'Success',
+        '1'  => 'Syntax or usage error',
+        '2'  => 'Protocol incompatibility',
+        '3'  => 'Errors selecting input/output files, dirs',
+        '4'  => 'Requested  action  not  supported',
+        '5'  => 'Error starting client-server protocol',
+        '6'  => 'Daemon unable to append to log-file',
+        '10' => 'Error in socket I/O  Error in file I/O',
+        '11' => 'Error in file I/O',
+        '12' => 'Error in rsync protocol data stream',
+        '13' => 'Errors with program diagnostics',
+        '14' => 'Error in IPC code',
+        '20' => 'Received SIGUSR1 or SIGINT',
+        '21' => 'Some error returned by waitpid()',
+        '22' => 'Error allocating core memory buffers',
+        '23' => 'Partial transfer due to error',
+        '24' => 'Partial transfer due to vanished source files',
+        '25' => 'The --max-delete limit stopped deletions',
+        '30' => 'Timeout in data send/receive',
+        '35' => 'Timeout waiting for daemon connection',
+    },
+};
 
 # dispatcher needs a handle to write to:
 our $DISPATCHER;
