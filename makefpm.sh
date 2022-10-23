7cabcf2aebc6cab7479818cf32a03217e653ff28
Aran Cox <aran-cox@uiowa.edu>
2022-10-23 07:25:59 -0500

    use fpm -s cpan for building package

--- makefpm.sh
+++ makefpm.sh
@@ -1,22 +1,21 @@
 #! /bin/bash
 #
 pkg=rdbduprunner
-rm -rf ./Build/*
-mkdir -p ./Build/{usr/bin,"usr/lib/${pkg}","usr/lib/tmpfiles.d","etc/${pkg}/conf.d","run/${pkg}","var/log/${pkg}"} 
+#rm -rf ./Build/*
+#mkdir -p ./Build/{usr/bin,"usr/lib/${pkg}","usr/lib/tmpfiles.d","etc/${pkg}/conf.d","run/${pkg}","var/log/${pkg}"} 
 
-cp $pkg ./Build/usr/bin/
-rsync -av check_mk "./Build/usr/lib/${pkg}"
-cp "contrib/tmpfiles.d/${pkg}.conf" ./Build/usr/lib/tmpfiles.d/
+#cp $pkg ./Build/usr/bin/
+#rsync -av check_mk "./Build/usr/lib/${pkg}"
+#cp "contrib/tmpfiles.d/${pkg}.conf" ./Build/usr/lib/tmpfiles.d/
 
 version=$(git tag -l | tail -1 | sed 's/^v//' )
 summary="script for making backups with rsync, rdiff-backup, and duplicity"
 description="runs backup programs per a configuration file"
-rpm_deps="-d perl-JSON -d perl-Log-Dispatch -d perl-Config-General -d perl-Readonly"
-deb_deps="-d libjson-perl -d liblog-dispatch-perl -d libconfig-general-perl -d libreadonly-perl"
+#rpm_deps="-d perl-JSON -d perl-Log-Dispatch -d perl-Config-General -d perl-Readonly"
+#deb_deps="-d libjson-perl -d liblog-dispatch-perl -d libconfig-general-perl -d libreadonly-perl"
 common_deps="-d perl -d rsync"
-common_opts="-n rdbduprunner --version "${version}" --architecture noarch -C ./Build -s dir"
+common_opts="--version ${version} --architecture noarch -s cpan --prefix /usr --replaces rdbduprunner --cpan-perl-lib-path /usr/share/perl5 ."
 
 fpm $common_opts $common_deps $rpm_deps -t rpm --rpm-summary "${summary}" --description "${description}" .
 fpm $common_opts $common_deps $deb_deps -t deb --rpm-summary "${summary}" --description "${description}" .
 
-fpm -s cpan -t rpm --prefix /usr --version 1.5 --replaces rdbduprunner --cpan-perl-lib-path /usr/share/perl5 .
