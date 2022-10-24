8ee591f7cfa0b525b7b5e00c56c55706240fcaf7
Aran Cox <aran-cox@uiowa.edu>
2022-10-24 13:13:03 -0500

    attempted fixes to package building

--- makefpm.sh
+++ makefpm.sh
@@ -19,10 +19,10 @@ rm -f $tmp
 summary="script for making backups with rsync, rdiff-backup, and duplicity"
 description="runs backup programs per a configuration file"
 #rpm_deps="-d perl-JSON -d perl-Log-Dispatch -d perl-Config-General -d perl-Readonly"
-#deb_deps="-d libjson-perl -d liblog-dispatch-perl -d libconfig-general-perl -d libreadonly-perl"
+deb_deps="-d libjson-perl -d liblog-dispatch-perl -d libconfig-general-perl -d libreadonly-perl -d libconfig-any-perl"
 common_deps="-d perl -d rsync"
-common_opts="--version ${version} --architecture noarch -s cpan --prefix /usr --replaces rdbduprunner --cpan-perl-lib-path /usr/share/perl5 ."
+common_opts="--version ${version} --architecture noarch -s cpan --prefix /usr --replaces rdbduprunner --cpan-perl-lib-path /usr/share/perl5"
 
-fpm $common_opts $common_deps $rpm_deps -t rpm --rpm-summary "${summary}" --description "${description}" .
-fpm $common_opts $common_deps $deb_deps -t deb --rpm-summary "${summary}" --description "${description}" .
+fpm --verbose $common_opts $common_deps $rpm_deps -t rpm --rpm-summary "${summary}" --description "${description}" .
+fpm --verbose $common_opts $deb_deps -t deb --no-auto-depends --rpm-summary "${summary}" --description "${description}" .
 
