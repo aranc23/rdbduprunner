635f37d88734387e082d8024ce2e4fe430c4c8d6
Aran Cox <arancox@gmail.com>
2022-10-27 12:00:03 -0500

    fixes to the build script

--- makefpm.sh
+++ makefpm.sh
@@ -3,13 +3,13 @@
 set -e
 umask 002
 pkg="rdbduprunner"
+build_dir=$(mktemp -d)
 
-rm -rf ./Build/*
-mkdir -p ./Build/{"usr/lib/${pkg}","usr/lib/tmpfiles.d","etc/${pkg}/conf.d","run/${pkg}","var/log/${pkg}"} 
+mkdir -p "${build_dir}"/{"usr/lib/${pkg}","usr/lib/tmpfiles.d","etc/${pkg}/conf.d","run/${pkg}","var/log/${pkg}"} 
 
 #cp $pkg ./Build/usr/bin/
-rsync -av check_mk "./Build/usr/lib/${pkg}"
-cp "contrib/tmpfiles.d/${pkg}.conf" ./Build/usr/lib/tmpfiles.d/
+rsync -av check_mk "${build_dir}/usr/lib/${pkg}"
+cp "contrib/tmpfiles.d/${pkg}.conf" "${build_dir}/usr/lib/tmpfiles.d/"
 
 tmp=$(mktemp)
 egrep 'our \$VERSION' lib/Backup/rdbduprunner.pm > $tmp
@@ -23,25 +23,29 @@ rm -f $tmp
 #deb_deps="--no-auto-depends -d libjson-perl -d liblog-dispatch-perl -d libconfig-general-perl -d libreadonly-perl -d libconfig-any-perl"
 deb_deps="-d liburi-perl"
 common_deps="-d perl -d rsync"
-common_opts="--version ${version} --iteration ${iteration} -m arancox@gmail.com --architecture noarch -s cpan --prefix /usr --replaces perl-backup-rdbduprunner --cpan-perl-lib-path /usr/share/perl5"
+common_opts="--version ${version} --iteration ${iteration} -m arancox@gmail.com --architecture noarch -s cpan --prefix /usr --replaces perl-backup-rdbduprunner --cpan-perl-lib-path /usr/share/perl5 --rpm-summary \"${summary}\" --description \"${description}\""
 
-cpan_reject=(Config vars warnings strict Encode Carp IO::Select IO::Handle Fcntl POSIX Sys::Hostname URI::Escape Scalar::Util AnyDBM_File File::Basename Data::Dumper Log::Dispatch::Screen Log::Dispatch::Syslog Log::Dispatch::File AnyDBM-File)
+cpan_reject=(Config vars warnings strict Encode Carp IO::Select IO::Handle Fcntl POSIX Sys::Hostname URI::Escape Scalar::Util File::Basename Data::Dumper Log::Dispatch::Screen Log::Dispatch::Syslog Log::Dispatch::File AnyDBM-File)
 for mod in ${cpan_reject[@]}; do
-    deb_deps="${deb_deps} --cpan-package-depend-reject ${mod}"
+    deb_deps="${deb_deps} --cpan-package-reject-from-depends ${mod}"
 done
 
 summary="script for making backups with rsync, rdiff-backup, and duplicity"
 description="runs backup programs per a configuration file"
 
-fpm -n $pkg --version ${version} --iteration ${iteration} -m arancox@gmail.com --architecture noarch -C ./Build -s dir -d perl-Backup-rdbduprunner    -t rpm --rpm-summary "${summary}" --description "${description}" .
-fpm -n $pkg --version ${version} --iteration ${iteration} -m arancox@gmail.com --architecture noarch -C ./Build -s dir -d libbackup-rdbduprunner-perl -t deb --rpm-summary "${summary}" --description "${description}" .
+fpm -n $pkg --version ${version} --iteration ${iteration} -m arancox@gmail.com --architecture noarch -C $build_dir -s dir -d perl-Backup-rdbduprunner    -t rpm --rpm-summary "${summary}" --description "${description}" .
+fpm -n $pkg --version ${version} --iteration ${iteration} -m arancox@gmail.com --architecture noarch -C $build_dir -s dir -d libbackup-rdbduprunner-perl -t deb --rpm-summary "${summary}" --description "${description}" .
+
+rm -rf $build_dir
 
 summary="module for making backups with rsync, rdiff-backup, and duplicity"
 #description="runs backup programs per a configuration file"
 
-fpm --verbose $common_opts $common_deps $rpm_deps -t rpm --rpm-summary "${summary}" --description "${description}" --name perl-Backup-rdbduprunner .
-fpm --verbose $common_opts $common_deps $deb_deps -t deb --cpan-package-name-prefix lib --rpm-summary "${summary}" --description "${description}" .
+# create the packages for the Backup::rdbduprunner module
+fpm --verbose $common_opts $common_deps $rpm_deps -t rpm .
+fpm --verbose $common_opts $common_deps $deb_deps -t deb --cpan-package-name-prefix lib --cpan-package-name-postfix -perl .
 
+# build two packages missing on Ubuntu 20.04 needed by Backup::rdbduprunner:
 for pkg in No::Worries Config::Validator; do
     fpm --verbose --iteration $iteration --prefix /usr --cpan-perl-lib-path /usr/share/perl5 -s cpan -t deb -m arancox@gmail.com $deb_deps $pkg
 done
