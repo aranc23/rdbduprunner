78dc2b87b618a112ec410fe6adc9bac30e6b47f3
Aran Cox <aran-cox@uiowa.edu>
2022-10-27 12:22:31 -0500

    more build fixes, skip tests, etc.

--- makefpm.sh
+++ makefpm.sh
@@ -4,7 +4,7 @@ set -e
 umask 002
 pkg="rdbduprunner"
 build_dir=$(mktemp -d)
-
+url=https://github.com/aranc23/rdbduprunner
 mkdir -p "${build_dir}"/{"usr/lib/${pkg}","usr/lib/tmpfiles.d","etc/${pkg}/conf.d","run/${pkg}","var/log/${pkg}"} 
 
 #cp $pkg ./Build/usr/bin/
@@ -16,36 +16,32 @@ egrep 'our \$VERSION' lib/Backup/rdbduprunner.pm > $tmp
 echo 'print $VERSION."\n"' >> $tmp
 
 version=$(perl $tmp)
-iteration=3
+iteration=6
 rm -f $tmp
 
 #rpm_deps="-d perl-JSON -d perl-Log-Dispatch -d perl-Config-General -d perl-Readonly"
 #deb_deps="--no-auto-depends -d libjson-perl -d liblog-dispatch-perl -d libconfig-general-perl -d libreadonly-perl -d libconfig-any-perl"
 deb_deps="-d liburi-perl"
 common_deps="-d perl -d rsync"
-common_opts="--version ${version} --iteration ${iteration} -m arancox@gmail.com --architecture noarch -s cpan --prefix /usr --replaces perl-backup-rdbduprunner --cpan-perl-lib-path /usr/share/perl5 --rpm-summary \"${summary}\" --description \"${description}\""
+summary="script and module for making backups with rsync, rdiff-backup, and duplicity"
+description="runs backup programs per a configuration file"
+common_opts="--version ${version} --iteration ${iteration} -m arancox@gmail.com --architecture noarch -s cpan --prefix /usr --replaces perl-backup-rdbduprunner --cpan-perl-lib-path /usr/share/perl5 --url ${url}"
 
 cpan_reject=(Config vars warnings strict Encode Carp IO::Select IO::Handle Fcntl POSIX Sys::Hostname URI::Escape Scalar::Util File::Basename Data::Dumper Log::Dispatch::Screen Log::Dispatch::Syslog Log::Dispatch::File AnyDBM-File)
 for mod in ${cpan_reject[@]}; do
     deb_deps="${deb_deps} --cpan-package-reject-from-depends ${mod}"
 done
 
-summary="script for making backups with rsync, rdiff-backup, and duplicity"
-description="runs backup programs per a configuration file"
-
-fpm -n $pkg --version ${version} --iteration ${iteration} -m arancox@gmail.com --architecture noarch -C $build_dir -s dir -d perl-Backup-rdbduprunner    -t rpm --rpm-summary "${summary}" --description "${description}" .
-fpm -n $pkg --version ${version} --iteration ${iteration} -m arancox@gmail.com --architecture noarch -C $build_dir -s dir -d libbackup-rdbduprunner-perl -t deb --rpm-summary "${summary}" --description "${description}" .
+fpm -n $pkg --version ${version} --iteration ${iteration} -m arancox@gmail.com --architecture noarch -C $build_dir -s dir -d perl-Backup-rdbduprunner    -t rpm --rpm-summary "${summary}" --description "${description}" --url "${url}" .
+fpm -n $pkg --version ${version} --iteration ${iteration} -m arancox@gmail.com --architecture noarch -C $build_dir -s dir -d libbackup-rdbduprunner-perl -t deb --rpm-summary "${summary}" --description "${description}" --url "${url}" .
 
 rm -rf $build_dir
 
-summary="module for making backups with rsync, rdiff-backup, and duplicity"
-#description="runs backup programs per a configuration file"
-
 # create the packages for the Backup::rdbduprunner module
-fpm --verbose $common_opts $common_deps $rpm_deps -t rpm .
-fpm --verbose $common_opts $common_deps $deb_deps -t deb --cpan-package-name-prefix lib --cpan-package-name-postfix -perl .
+fpm --verbose --no-cpan-test $common_opts $common_deps $rpm_deps -t rpm --rpm-summary "${summary}" --description "${description}" .
+fpm --verbose --no-cpan-test $common_opts $common_deps $deb_deps -t deb --rpm-summary "${summary}" --description "${description}" --cpan-package-name-prefix lib --cpan-package-name-postfix -perl .
 
 # build two packages missing on Ubuntu 20.04 needed by Backup::rdbduprunner:
 for pkg in No::Worries Config::Validator; do
-    fpm --verbose --iteration $iteration --prefix /usr --cpan-perl-lib-path /usr/share/perl5 -s cpan -t deb -m arancox@gmail.com $deb_deps $pkg
+    fpm --verbose --no-cpan-test --iteration $iteration --prefix /usr --cpan-perl-lib-path /usr/share/perl5 -s cpan -t deb -m arancox@gmail.com $deb_deps $pkg
 done
