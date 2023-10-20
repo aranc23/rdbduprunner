c91c877a6647b458f8ff354eb7d24a39fc819f34
Aran Cox <aran-cox@uiowa.edu>
2023-10-20 13:45:30 -0500

    bump version to 2

--- lib/Backup/rdbduprunner.pm
+++ lib/Backup/rdbduprunner.pm
@@ -117,7 +117,7 @@ our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = qw(
 );
 
-our $VERSION = '1.9.5';
+our $VERSION = '2.0.0';
 
 # constant name of the application
 our $APP_NAME = basename($0);
