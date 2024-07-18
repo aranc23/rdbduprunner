b2ee6ae6a17c42b54c6206a10ca7ae82520132f5
Aran Cox <aran-cox@uiowa.edu>
2024-07-18 13:14:42 -0500

    bump version for check_mk plugin changes

--- lib/Backup/rdbduprunner.pm
+++ lib/Backup/rdbduprunner.pm
@@ -117,7 +117,7 @@ our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = qw(
 );
 
-our $VERSION = '2.0.6';
+our $VERSION = '2.0.7';
 
 # constant name of the application
 our $APP_NAME = basename($0);
